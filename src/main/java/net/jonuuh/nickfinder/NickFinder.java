8ff7afa8cf81386262f3f91a35565fc1e527dcce
jonuuh <jhammond.123x@gmail.com>
2023-12-26 18:30:53 -0800

    edit template

--- src/main/java/net/jonuuh/nickfinder/NickFinder.java
+++ src/main/java/net/jonuuh/nickfinder/NickFinder.java
@@ -1,14 +1,14 @@
-package net.example.examplemod;
+package net.jonuuh.nickfinder;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = NickFinder.MODID, version = NickFinder.VERSION)
+public class NickFinder
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "nickfinder";
     public static final String VERSION = "1.0";
     
     @EventHandler
